79e6fd7944df7def9ff42cfeb4ac04de4083926b
Katacoda Scenario <scenario@katacoda.com>
2021-02-16 04:54:40 +0000

    Solve test-lab
    Signed-off-by: Katacoda Scenario <scenario@katacoda.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+  
+  if len(os.Args) > 1 {
+    names := os.Args[1:] 
+    fullName := strings.Join(names, " ")
+  	fmt.Println("Hello "+fullName+", Welcome to the jungle")
+  } else {
+    fmt.Println("Error message ")
+  }
 }
